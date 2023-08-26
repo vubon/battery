8c1ed782656b38615611a49f780f032f77028c3f
vubon <vubon.roy@gmail.com>
2023-08-26 12:54:33 +0700

    Update test cases

--- src/test/java/com/battery/BatteryControllerTests.java
+++ src/test/java/com/battery/BatteryControllerTests.java
@@ -22,6 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -60,8 +61,19 @@ public class BatteryControllerTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(batteries)))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
 
-        // Add more assertions based on your requirements
+    @Test
+    public void testCreateBatteriesWithoutBody() throws Exception {
+        // Generate a JWT token using your JwtHelper class
+        UserDetails userDetails = userDetailsService.loadUserByUsername("vubon");
+        String token = jwtHelper.generateToken(userDetails);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/batteries")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Should have request body")));
     }
 
     @Test
@@ -75,7 +87,7 @@ public class BatteryControllerTests {
         batteries.add(new Battery("Battery 1", "12345", 1000));
         batteries.add(new Battery("Battery 2", "67890", 2000));
         // Mock batteryService.getBatteriesByPostcodeRange to return the mock batteries
-        Mockito.when(batteryService.findBatteriesByPostcodeRange(Mockito.anyString(), Mockito.anyString()))
+        Mockito.when(batteryService.getBatteriesByPostcodeRange(Mockito.anyString(), Mockito.anyString()))
                 .thenReturn(batteries);
 
         // Perform GET request
@@ -86,7 +98,53 @@ public class BatteryControllerTests {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.batteryInfoList", Matchers.hasSize(2)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.statistics.totalCapacity", Matchers.is(3000)));
+    }
 
-        // Add more assertions based on your requirements
+    @Test
+    public void testGetBatteriesWithinPostcodeAndCapacityRange() throws Exception {
+        // Generate a JWT token using your JwtHelper class
+        UserDetails userDetails = userDetailsService.loadUserByUsername("vubon");
+        String token = jwtHelper.generateToken(userDetails);
+
+        // Prepare mock data
+        List<Battery> batteries = new ArrayList<>();
+        batteries.add(new Battery("Battery 1", "12345", 1000));
+        batteries.add(new Battery("Battery 2", "67890", 2000));
+        // Mock batteryService.getBatteriesByPostcodeRange to return the mock batteries
+        Mockito.when(batteryService.getBatteriesByPostcodeRangeAndCapacity(
+                        Mockito.anyInt(), Mockito.anyInt(),
+                        Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(batteries);
+
+        // Perform GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/batteries")
+                        .header("Authorization", "Bearer " + token)
+                        .param("minCapacity", "1000")
+                        .param("maxCapacity", "2000")
+                        .param("startPostcode", "12345")
+                        .param("endPostcode", "67890"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.batteryInfoList", Matchers.hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.statistics.totalCapacity", Matchers.is(3000)));
+    }
+
+    @Test
+    public void testGetBatteriesWhenNoParams() throws Exception {
+        // Generate a JWT token using your JwtHelper class
+        UserDetails userDetails = userDetailsService.loadUserByUsername("vubon");
+        String token = jwtHelper.generateToken(userDetails);
+
+        // Prepare mock data
+        List<Battery> batteries = new ArrayList<>();
+        batteries.add(new Battery("Battery 1", "12345", 1000));
+        batteries.add(new Battery("Battery 2", "67890", 2000));
+        // Mock batteryService.getBatteriesByPostcodeRange to return the mock batteries
+        Mockito.when(batteryService.getBatteriesByPostcodeRange(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(batteries);
+
+        // Perform GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/batteries")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
